04286100ad49dc1f967ab7d5784e02f29918bbeb
lxndrbnsv <abnosov@gmail.com>
2021-10-02 09:59:55 +0300

    removed user agent

--- src/models/translate.ts
+++ src/models/translate.ts
@@ -2,7 +2,7 @@ export function translate(msg, language) {
     const apiURL = "https://qwertynetworks.com/translateit.php";
     var xhr = new XMLHttpRequest();
     xhr.open("POST", apiURL, false);
-    xhr.setRequestHeader("User-Agent", "Cron");
+    // xhr.setRequestHeader("User-Agent", "Cron");
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.send(`phrase=${msg}&lng=${language}`);
     return (
